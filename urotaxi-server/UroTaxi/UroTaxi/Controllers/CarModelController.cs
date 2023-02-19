6c59bb3f92005e9ecaabf657c74963a264079c0b
akhilpvijayan <akhilpv88.apg@gmail.com>
2023-02-19 20:41:48 +0530

    Uro-Taxi : Added cars module

---
+++ urotaxi-server/UroTaxi/UroTaxi/Controllers/CarModelController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UroTaxi.Business.Services;
+using UroTaxi.Entities;
+
+namespace UroTaxi.Controllers
+{
+    [ApiController]
+    public class CarModelController : ControllerBase
+    {
+
+        #region Private Functions
+        private readonly ICityService _cityService;
+        private readonly ApplicationDBContext _applicationDbContext;
+        #endregion
+
+        #region Constructors
+        public CarModelController(
+            ICityService cityService,
+            ApplicationDBContext applicationDbContext)
+        {
+            _cityService = cityService;
+            _applicationDbContext = applicationDbContext;
+        }
+        #endregion
+
+        #region Public Functions
+        [HttpGet]
+        [Route("carmodel")]
+        [ProducesResponseType(typeof(CarModel), 200)]
+        [ProducesResponseType(404)]
+        public Task<List<CarModel>> GetAllCarModels()
+        {
+            return _applicationDbContext.CarModels.ToListAsync();
+        }
+        #endregion
+    }
+}
